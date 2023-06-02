a3f80959fee14723287b57d515866cb456de0109
bambi01-95 <bambi0905hs@ezweb.ne.jp>
2023-06-02 16:19:51 +0900

    Initial Commit

---
+++ mecha_test/mecha_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  mecha_testApp.swift
+//  mecha_test
+//
+//  Created by Hiroto SHIKADA on 2023/06/02.
+//
+
+import SwiftUI
+
+@main
+struct mecha_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
